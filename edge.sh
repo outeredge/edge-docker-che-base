7cdba8caf861324155ff0e6b69010c29799c087b
David Windell <david@outeredgeuk.com>
2017-10-13 14:57:22 +0100

    Revert uid change

--- edge.sh
+++ edge.sh
@@ -2,10 +2,6 @@
 
 DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
 
-sudo userdel -f www-data
-
 sudo groupadd -for -g ${DOCKER_GID} docker
 sudo usermod -aG docker user
-
-#sudo usermod -g www-data user
-sudo usermod -u 33 user
+sudo usermod -aG www-data user
