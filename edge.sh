867ebedb81d2999d651edc7744cde7ce778a105c
David Windell <david@outeredgeuk.com>
2018-05-03 12:42:51 +0100

    Remove env for now

--- edge.sh
+++ edge.sh
@@ -2,8 +2,6 @@
 
 DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
 
-sudo env > /etc/environment
-
 sudo groupadd -for -g ${DOCKER_GID} docker
 sudo usermod -aG docker user
 sudo usermod -aG www-data user
