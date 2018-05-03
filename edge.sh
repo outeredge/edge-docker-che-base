2ac6ad002a1c6f2a38f95e4130852eaa47a0e24f
David Windell <david@outeredgeuk.com>
2018-05-03 13:04:15 +0100

    Use tee to retain use env

--- edge.sh
+++ edge.sh
@@ -2,6 +2,8 @@
 
 DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
 
+env | sudo tee /etc/environment
+
 sudo groupadd -for -g ${DOCKER_GID} docker
 sudo usermod -aG docker user
 sudo usermod -aG www-data user
