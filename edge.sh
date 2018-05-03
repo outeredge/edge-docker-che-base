4942fe2dac30c07c7d7e600f169714f5287e463a
David Windell <david@outeredgeuk.com>
2018-05-03 11:53:54 +0100

    Add environment variables permanently (for SSH)

--- edge.sh
+++ edge.sh
@@ -1,10 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
 
+env > /etc/environment
+
 sudo groupadd -for -g ${DOCKER_GID} docker
 sudo usermod -aG docker user
-
 sudo usermod -aG www-data user
 
 mkdir -p ~/.ssh
